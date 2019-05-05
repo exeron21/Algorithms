57f6abb5f7f7f6e52e9fad53b33a240d400135b0
will <ooops@126.com>
2019-05-05 09:19:57 +0800

    将true替换成2在代码中是不可能做到的，应该是用asmtools直接修改字节码来完成

--- src/jvm/Foo.java
+++ src/jvm/Foo.java
@@ -3,7 +3,7 @@ package jvm;
 public class Foo {
     static boolean boolValue;
     public static void main(String[] args) {
-        boolValue = true; // 将这个 true 替换为 2 或者 3，再看看打印结果
+        boolValue = true;
         if (boolValue) System.out.println("Hello, Java!");
         if (boolValue == true) System.out.println("Hello, JVM!");
     }
